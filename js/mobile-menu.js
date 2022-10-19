d0b4ef5604ee4d092c58eaf5ba0be6b8901677e6
natalynoele <natalynoele@gmail.com>
2022-10-19 14:40:59 +0300

    mobile menu

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -2,14 +2,19 @@
   const mobileMenu = document.querySelector('.js-menu-container');
   const openMenuBtn = document.querySelector('.js-open-menu');
   const closeMenuBtn = document.querySelector('.js-close-menu');
+  const body = document.querySelector("body");
 
   const toggleMenu = () => {
     const isMenuOpen = openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
     openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
     mobileMenu.classList.toggle('is-open');
 
-    const scrollLockMethod = !isMenuOpen ? 'disableBodyScroll' : 'enableBodyScroll';
-    bodyScrollLock[scrollLockMethod](document.body);
+    if (!isMenuOpen) {
+     body.classList.add('no-scroll');    
+    } else {
+      body.classList.remove('no-scroll');  
+    }
+   
   };
 
   openMenuBtn.addEventListener('click', toggleMenu);
